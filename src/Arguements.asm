51e5734dd3913fb162a1ca7b3690ef02af0cae18
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2022-05-10 11:14:27 -0400

    Update Arguements.asm

--- src/Arguements.asm
+++ src/Arguements.asm
@@ -1,4 +1,9 @@
+section .data
+    cmdlineFile: "/proc/self/cmdline"
 section .text
     global getArguments
 getArguments:
-    mov 
+    mov 02h, %rax
+    mov cmdlineFile, %rdi
+    mov 0700, %rsi
+    int 80h
