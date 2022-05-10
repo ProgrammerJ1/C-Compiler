ea795145930ad84b3655bbd03a1665b386ee0706
ProgrammerJ1 <scotte1953@gmail.com>
2022-05-10 18:30:45 -0400

    Got arguments

--- src/Arguements.asm
+++ src/Arguements.asm
@@ -3,12 +3,15 @@ section .data
 section .text
     global getArguments
 getArguments:
+    .bss arguementsBuffer, cmdlineSize
+    .long ArgumentIndexesAmount 0
+    .long ArgumentIterator 0
+    .long ArgumentsBufferMaxIndex cmdlineSize
     mov %rax, 04h
     mov %rdi, cmdlineFilepath
     mov %rsi,cmdlineStat
     int 80h
     mov cmdlineSize, cmdlineStat[.size]
-    .bss arguementsBuffer, cmdlineSize
     mov %rax, 02h
     mov %rdi, cmdlineFilepath
     mov %rsi, 0700
@@ -19,7 +22,22 @@ getArguments:
     mov %rsi, arguementsBuffer
     mov %rdx, cmdlineSize
     int 80h
-    
+    mov %rax, 03h
+    mov %rdi, [cmdlineFile]
+    int 80h
+    dec ArgumentsBufferMaxIndex
+    findAllNulls:
+        findifCharNull:
+            cmp arguementsBuffer[ArgumentIterator], 00h
+            JE updateArgumentIndexesAmount
+            updateArgumentIndexesAmount:
+                inc ArgumentIndexesAmount
+        cmp ArgumentIterator,ArgumentsBufferMaxIndex
+        JE updateIteratorandRestart
+        updateIteratorandRestart:
+            add ArgumentIterator, 1
+            jmp findAllNulls
+    .long argument
 section .bss
     cmdlineStat: resb 88
     cmdlineSize: resb 8
