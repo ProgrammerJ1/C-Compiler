dc27a78988231d29b456ec6855f53c2b1d188dce
ProgrammerJ1 <scotte1953@gmail.com>
2022-05-10 16:27:28 -0400

    Finished file operations

--- src/Arguements.asm
+++ src/Arguements.asm
@@ -4,13 +4,22 @@ section .text
     global getArguments
 getArguments:
     mov %rax, 04h
-    mov 
+    mov %rdi, cmdlineFilepath
+    mov %rsi,cmdlineStat
     int 80h
+    mov cmdlineSize, cmdlineStat[.size]
+    .bss arguementsBuffer, cmdlineSize
     mov %rax, 02h
     mov %rdi, cmdlineFilepath
     mov %rsi, 0700
     int 80h
     mov [cmdlineFile], %rax
-    mov 
+    mov %rax, 00h
+    mov %rdi, [cmdlineFile]
+    mov %rsi, arguementsBuffer
+    mov %rdx, cmdlineSize
+    int 80h
+    
 section .bss
     cmdlineStat: resb 88
+    cmdlineSize: resb 8
