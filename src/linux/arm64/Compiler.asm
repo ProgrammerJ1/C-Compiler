b075e0e0b048e3ced33e7b8de2dba3346bbb8f41
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2022-05-12 12:54:40 -0400

    Update Compiler.asm
    Created iterators

--- src/linux/arm64/Compiler.asm
+++ src/linux/arm64/Compiler.asm
@@ -6,7 +6,24 @@ section .text
     int 80h
   getArguments:
     mov argc, %r0
-    mov
+    sub 1, argc
+    mov finalArgIterator, argc
+    add 2, finalArgIterator
+    mov agrv, %r1
+    cmp finalArgIterator,2
+    mov currentArgIterator, firstArgIterator
+    ja findArgSizes
+    findArgSizes:
+      jmp init
+      init:
+        mov %r2, 4
+      loop:
+        mov %r3, currentArgIterator
+        mul %r4, %r2, %r3
 section .bss
   argc: resd 1
   finalArgIterator: resd 1
+  currentArgIterator: resq 1
+  currentTrueArgIterator: resq 1
+  argumentPointers: resq 8
+  argumentsArraySize: resq 1
