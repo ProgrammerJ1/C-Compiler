68cfc8567b07bff8afbf26f7bf9e5578c0da62c4
ProgrammerJ1 <scotte1953@gmail.com>
2022-05-12 17:44:48 -0400

    Added pop and deleted arg iterator

--- src/linux/x64/Compiler.asm
+++ src/linux/x64/Compiler.asm
@@ -4,22 +4,11 @@ section .text
     global _start
 _start:
     call getArguments
-getArguments:
-    mov argc, (%rsp)
+getArgumentCount:
+    pop argc
+    sub %rbp, 4
     sub argc, 1;Instructions to find number of arguments
-    mov argEndIterator, argc
-    add argEndIterator, 2;Functions to get End Argument Iterator
-    cmp argEndIterator,2
-    jne findargsSize
-    findargsSize:
-        mov %rax, currentargIterator
-        mov %rcx, 8
-        imul, %rcx
-        mov 
-        mov %rdi, [(%rsp)
+    ret
 section .bss
     argc: resq 1
     argvSize: resq 1
-    argEndIterator: resq 1
-    currentargIterator: resq 1
-    currentTrueArgIterator: resq 1
