c13d3491df1300bfe81f2537d4d85a36f16a24e3
ProgrammerJ1 <scotte1953@gmail.com>
2022-05-13 10:01:48 -0400

    Adding HelpOption

--- src/linux/x64/Compiler.asm
+++ src/linux/x64/Compiler.asm
@@ -1,12 +1,15 @@
 section .data
     argStartIterator: db 2
-    noFilesErrorMessage: "Error: No Files Specified"
+    noFilesErrorMessage: db "Error: No Files Specified"
+    HelpOne: db "-h"
+    HelpTwo:"--help"
 section .text
     global _start
 _start:
     call getArgumentCount
     cmp argc,0
     je NoInputFileError
+    jne Program
     NoInputFileError:
         mov %rax, 0
         mov %rdi, 1
@@ -16,6 +19,12 @@ _start:
         mov %rax, 3ch
         mov %rdi, 22
         int 80h
+    Program:
+        pop Destroyer
+        cmp %esp,c
+        mov HelpOptionPlacholderOne, ZF
+        cmp %esp, HelpTwo
+        mov HelpOptionPlacholderTwo, ZF
 getArgumentCount:
     pop argc
     sub %rbp, 4
@@ -23,3 +32,6 @@ getArgumentCount:
     ret
 section .bss
     argc: resq 1
+    Destroyer: resb 0
+    HelpOptionPlacholderOne:resb 1
+    HelpOptionPlacholderTwo: resb 1
