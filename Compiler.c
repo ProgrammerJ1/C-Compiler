4e3a1aba79249672d7f36c062918f884f7b38e05
ProgrammerJ1 <scotte1953@gmail.com>
2022-05-14 15:42:00 -0400

    Edited flag checker and added flag type

--- Compiler.c
+++ Compiler.c
@@ -4,5 +4,18 @@ int main(int argc,char** argv) {
     return 0;
 }
 bool isFlag(char* value) {
-    return strcmp(value,"-h")||strcmp(value,"--help")||strcmp(value,"-v")||strcmp(value,"--version")||strcmp(value,"-a")||strcmp(value,"-s")||strcmp(value,"-o");
+    return strcmp(value,"-h")||strcmp(value,"--help")||strcmp(value,"-v")||strcmp(value,"--version")||strcmp(value,"-a")||strcmp(value,"-c")||strcmp(value,"-p");
+}
+int FlagType(char* Flag) {
+    if (strcmp(Flag,"-h")||strcmp(Flag,"--help")) {
+        return 0;
+    } else if (strcmp(Flag,"-v")||strcmp(Flag,"--version")) {
+        return 1;
+    } else if (strcmp(Flag,"-p")) {
+        return 2;
+    } else if (strcmp(Flag,"-c")) {
+        return 3;
+    } else if (strcmp(Flag,"-a")) {
+        return 4;
+    }
 }
