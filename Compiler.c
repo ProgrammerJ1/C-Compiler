751ab62d917b7896979d51460be1948f82225be3
ProgrammerJ1 <scotte1953@gmail.com>
2022-05-14 15:30:37 -0400

    Created main function

--- Compiler.c
+++ Compiler.c
@@ -1,1 +1,8 @@
 #include <string.h>
+#include <stdbool.h>
+int main(int argc,char** argv) {
+    return 0;
+}
+bool isFlag(char* value) {
+    if (value=="-h"||value=="--help"||value=="-v"||value=="--version"||value=="-a"||value="-s"||value="-o")
+}
