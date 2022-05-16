dbd20e3d9fa623e336c9b6f20f5124751b96cd3b
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2022-05-16 12:52:38 -0400

    Update CompilerInfoGetter.h
    Added registers struct

--- src/linux/CompilerInfoGetter.h
+++ src/linux/CompilerInfoGetter.h
@@ -1,1 +1,12 @@
 #include <stdio.h>
+#include <string.h>
+struct Register {
+  char* Name;
+  char* Type;
+  size_t Bits;
+}
+Register* getRegisters(char* Arch) {
+  FILE* RegistersListFile;
+  char RegistersListText[39+strlen(Arch)]="/etc/newgcc/compilerinfo/";
+  strcat(RegistersListText,Arch,strlen(Arch));
+  strcat(RegistersListText,"/registers.list",strlen("/registers.list"));
