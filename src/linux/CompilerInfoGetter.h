78ecd2fb3cbd0d4fc992bdab92443a77270ebb64
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2022-05-16 14:28:27 -0400

    Update CompilerInfoGetter.h
    Added RegisterDataLists

--- src/linux/CompilerInfoGetter.h
+++ src/linux/CompilerInfoGetter.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include
 struct Register {
   char* Name;
   char* Type;
@@ -7,6 +8,14 @@ struct Register {
 }
 Register* getRegisters(char* Arch) {
   FILE* RegistersListFile;
-  char RegistersListText[39+strlen(Arch)]="/etc/newgcc/compilerinfo/";
-  strcat(RegistersListText,Arch,strlen(Arch));
-  strcat(RegistersListText,"/registers.list",strlen("/registers.list"));
+  char RegistersListFilePath[39+strlen(Arch)]="/etc/newgcc/compilerinfo/";
+  strcat(RegistersListFilePath,Arch,strlen(Arch));
+  strcat(RegistersListFilePath,"/registers.list",strlen("/registers.list"));
+  RegistersListFile=fopen(RegistersListFilePath,"r");
+  fseek(RegistersListFile,0,SEEK_END);
+  long RegistersFileListSize=ftell(RegistersListFile)
+  char* RegistersListText=fread(RegistersListFile,0,RegistersFileListSize);
+  char[2][] RegisterDataTextList;
+  RegistersListTextIndex=0;
+  for (int i=0;i<strlen(RegistersListText);i++) {
+    d
