70a975e032afb8c2b681dc637cf8b3cf24b48daf
ProgrammerJ1 <96920201+ProgrammerJ1@users.noreply.github.com>
2022-05-16 11:21:23 -0400

    Create CompilerInfoGetter.py
    Added Compiler Info Getter

---
+++ src/linux/CompilerInfoGetter.py
@@ -0,0 +1,15 @@
+import yaml
+import platform
+import cytpes
+import types
+def GetInfo(Arch: string):
+  InfoArray=[[],[],[]]
+  with open("/etc/newgcc/"+Arch+"compilerinfo.yaml","r") as info
+            InfoDocument=yaml.safe_load(info)
+            for i in InfoDocument["Registers"]:
+              InfoArray[0].append(c_char_p(i))
+            for i in InfoDocument["Commands"]:
+              InfoArray[1].append(c_char_p(i))
+            for i in InfoDocument["Syntax"]:
+              InfoArray[2].append(c_char_p(i))
+  return InfoArray
